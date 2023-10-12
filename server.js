8759d16ded254cd47c18c2890a78b2395021a24a
youyinnn <youyinnn@gmail.com>
2023-10-12 18:08:51 -0400

    change to node;

---
+++ server.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const express = require("express");
+
+// Constants
+const PORT = 8080;
+const HOST = "0.0.0.0";
+
+// App
+const app = express();
+app.use(express.json()); // <==== parse request body as JSON
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.json(req.body); // <==== req.body will be a parsed JSON object
+});
+
+app.listen(PORT, HOST, () => {
+  console.log(`Running on http://${HOST}:${PORT}`);
+});
